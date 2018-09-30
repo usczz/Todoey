1b28e835ed9f19fe5a8c7cca865acf942e32ed12
zhitao zhou <davidzhou0621@gmail.com>
2018-09-30 16:01:43 -0700

    Add new item functionality complete

--- Todoey/ToDoListViewController.swift
+++ Todoey/ToDoListViewController.swift
@@ -10,7 +10,7 @@ import UIKit
 
 class ToDoListViewController: UITableViewController {
     
-    let itemArray = ["Find Mike", "Buy milk", "Destroy"]
+    var itemArray = ["Find Mike", "Buy milk", "Destroy"]
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -33,14 +33,34 @@ class ToDoListViewController: UITableViewController {
     //MARK - TableView Delegate Methods
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         let currentCell = tableView.cellForRow(at: indexPath)
-        if currentCell?.accessoryType == .none {
-            currentCell?.accessoryType = .checkmark
-        } else {
+        if currentCell?.accessoryType == .checkmark {
             currentCell?.accessoryType = .none
+        } else {
+            currentCell?.accessoryType = .checkmark
         }
         
         tableView.deselectRow(at: indexPath, animated: true)
     }
+    
+    //MARK - Add New Item Button
+    @IBAction func addButtonPressed(_ sender: UIBarButtonItem) {
+        var textField = UITextField()
+        
+        let alert = UIAlertController(title: "Add new Todoey item", message: "", preferredStyle: .alert)
+        let action = UIAlertAction(title: "Add Item", style: .default) { (action) in
+            self.itemArray.append(textField.text!)
+            self.tableView.reloadData()
+        }
+        
+        alert.addTextField { (alertTextField) in
+            alertTextField.placeholder = "Create new item"
+            textField = alertTextField
+        }
+        
+        alert.addAction(action)
+        present(alert, animated: true, completion: nil)
+    }
+    
     
 }
 
